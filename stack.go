bb5f90684336d8a670f8c724c57b76b0e5c5ba56
mamkad <89218729+mamkad@users.noreply.github.com>
2021-10-04 16:53:55 +0300

    Update stack.go

--- stack.go
+++ stack.go
@@ -1,12 +1,3 @@
-/******************************************************************************
-
-                            Online Go Lang Compiler.
-                Code, Compile, Run and Debug Go Lang program online.
-Write your code in this editor and press "Run" button to execute it.
-
-*******************************************************************************/
-
-
 package main
 import ("fmt"; "sync")
 
